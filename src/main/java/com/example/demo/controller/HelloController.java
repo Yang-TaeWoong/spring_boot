cdfa119aeb32819b5b9478243e47e380a18aab06
TaeWoong_Yang <ytw5754@naver.com>
2021-04-13 23:34:07 +0900

    api, template, string passing controller with spring boot

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,12 +3,50 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+    //
     @GetMapping("hello")
-    public String hello(Model model){
-        model.addAttribute("data","hello");
+    public String hello(Model model) {
+        model.addAttribute("data", "hello");
         return "hello";
+    }
+
+    //    name을 도메인에서 입력받아서 hello-template라는 뷰를 넘김
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    //    hello + name 그냥 그대로 넘어감
+    @GetMapping("hello_spring")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+// 이렇게 날리면 hello 객체를 json형태로 날리는데, key를 name으로 value를 name으로 날림
+// 객체가 오면, json을 만들어서 반환하는게 기본적인 default 셋팅임.
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
